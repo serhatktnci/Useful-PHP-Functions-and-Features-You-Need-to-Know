84ddac718850be330a086a0d26cf758db4a9a271
serhatktnci <serhat.ketenci0@gmail.com>
2017-09-19 11:27:48 -0700

    Update Func.php
    PHP FUNCTION TO HELP PREVENT SQL INJECTION

--- Func.php
+++ Func.php
@@ -29,6 +29,7 @@ class Func {
         return trim($title, $space);
     }
 
+    
     public static function textToUtf8($title) {
 
         if (function_exists('iconv')) {
@@ -290,4 +291,56 @@ class Func {
         $text = "$difference $periods[$j] $ending";
         return $text;
     }
+    
+       
+  public static  function InputClean($input)
+ 
+{
+ 
+	if (is_array($input))
+ 
+	{
+ 
+		foreach ($input as $key => $val)
+ 
+		 {
+ 
+			$output[$key] = clean($val);
+ 
+			// $output[$key] = $this->clean($val);
+ 
+		}
+ 
+	}
+ 
+	else
+ 
+	{
+ 
+		$output = (string) $input;
+ 
+		// if magic quotes is on then use strip slashes
+ 
+		if (get_magic_quotes_gpc()) 
+ 
+		{
+ 
+			$output = stripslashes($output);
+ 
+		}
+ 
+		// $output = strip_tags($output);
+ 
+		$output = htmlentities($output, ENT_QUOTES, 'UTF-8');
+ 
+	}
+ 
+// return the clean text
+ 
+	return $output;
+ 
+}
+    
+    
+ 
 }
